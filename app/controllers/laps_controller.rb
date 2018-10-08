8e49b9c18ba7cf407b5dca0f4e08faa20e43efb6
Bárbara Chesman <chesman12@gmail.com>
2018-10-07 23:16:24 -0300

    Melhor volta da corrida

--- app/controllers/laps_controller.rb
+++ app/controllers/laps_controller.rb
@@ -4,10 +4,37 @@ class LapsController < ApplicationController
   end
   def index
   	@laps = Lap.all
-# 
-#     @teste += [@laps.nome_piloto.to_s]
-# @teste.save
+    @total_voltas = 4
+    #Pilotos
+    @pilotos = Lap.all.collect {|p| p.nome_piloto}
+    @pilotos = @pilotos.uniq
+    puts @pilotos
+
+    #Descobrir a melhor volta da corrida
+    melhorVolta = Lap.all.collect {|p| p.tempo_volta.strftime("%H:%M:%S:%L")}
+    sort = melhorVolta.sort
+    @menor = sort[0]
+
+    # @laps.each_with_object({}) do |nome_piloto, id_volta|
+    #   nome_piloto, id_volta = laps.values_at(:nome_piloto, :id_volta)
+    #   puts nome_piloto
+    # end
+
+    # @piloto_menor = @laps.select { |attachment| attachment.tempo_volta.strftime("%H:%M:%S:%L") == menor}
+    # puts menor
+    # puts @piloto_menor
+
+    #Descobrir a melhor volta de cada piloto
+  
+
+
+    #Calcular a velocidade média de cada piloto durante toda corrida
+
+    #Descobrir quanto tempo cada piloto chegou após o vencedor
+
+
   end
+
   def import
   	if params[:file]
   		Lap.import(params[:file])
